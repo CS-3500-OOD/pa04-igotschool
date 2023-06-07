23a89cbed4a570cf51cb7590d472979130c9e9a5
carolinetpham <carolinetpham@outlook.com>
2023-06-07 12:52:46 -0400

    records

--- src/main/java/cs3500/pa04/model/JoinMessage.java
+++ src/main/java/cs3500/pa04/model/JoinMessage.java
@@ -2,7 +2,6 @@ package cs3500.pa04.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record JoinMessage(
-    @JsonProperty("name") String name,
-    @JsonProperty("game-type") String gameType) {
+public record JoinMessage(@JsonProperty("name") String name, @JsonProperty("game-type") String gameType) {
+
 }
