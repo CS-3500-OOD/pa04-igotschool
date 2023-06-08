ce565c1dbd9ca85fa140c56014f6b89f6f542601
marbleville <allen.ehrhardt@gmail.com>
2023-06-07 20:46:24 -0400

    updated main

--- src/main/java/cs3500/pa04/Driver.java
+++ src/main/java/cs3500/pa04/Driver.java
@@ -1,8 +1,13 @@
 package cs3500.pa04;
 
 
+import cs3500.pa04.controller.ProxyController;
 import cs3500.pa04.model.BattleSalvoGame;
+import cs3500.pa04.model.ComputerPlayer;
+import cs3500.pa04.model.Player;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Socket;
 
 /**
  * This is the main driver of this project.
@@ -14,8 +19,26 @@ public class Driver {
    * @param args - no command line args required
    */
   public static void main(String[] args) {
-    BattleSalvoGame game = new BattleSalvoGame(new InputStreamReader(System.in));
-    game.setup(System.out, new InputStreamReader(System.in), new InputStreamReader(System.in));
-    game.playGame(System.out);
+    if (args.length == 0) {
+      BattleSalvoGame game = new BattleSalvoGame(new InputStreamReader(System.in));
+      game.setup(System.out, new InputStreamReader(System.in), new InputStreamReader(System.in));
+      game.playGame(System.out);
+    } else if (args.length == 2) {
+      try {
+        Player player = new ComputerPlayer(System.out);
+
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        Socket socket = new Socket(host, port);
+
+        ProxyController proxyController = new ProxyController(player, socket);
+
+        proxyController.run();
+      } catch (IOException e) {
+        System.out.println("Error connecting to server");
+      }
+    } else {
+      System.out.println("Invalid number of arguments");
+    }
   }
 }
