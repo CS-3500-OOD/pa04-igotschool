ce69091e1983e215594a71acf903b28ed9f41439
carolinetpham <carolinetpham@outlook.com>
2023-06-09 13:23:58 -0400

    Deleting unnecessary records

--- src/main/java/cs3500/pa04/json/EndGameMessage.java
+++
@@ -1,7 +0,0 @@
-package cs3500.pa04.json;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import cs3500.pa04.model.GameResult;
-
-public record EndGameMessage(@JsonProperty("end-game") GameResult result, String reason) {
-}
